6bdd54583fe6f9752dd426d79678f0615b4eceab
Ajeesh-Nair <ajeeshnair04@gmail.com>
2023-12-16 18:37:58 +0530

    Day 1

--- src/main/java/org/casperproject/Main.java
+++ src/main/java/org/casperproject/Main.java
@@ -1,7 +1,14 @@
 package org.casperproject;
 
+import org.testng.annotations.Test;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Test
+    public void TestPass(){
+        System.out.println("Pass");
     }
 }
